73c2aa6a88ee11c9cb359111c9937be0527b7ea5
James Hobson <jam.hobson@hotmail.co.uk>
2016-12-24 17:09:07 +0000

    Finished getLessonIndex()

--- freepc/src/timetable.c
+++ freepc/src/timetable.c
@@ -6,7 +6,18 @@ int getLessonIndex() {
     int index = 0;
     time (&rawtime);
     tminfo = localtime (&rawtime);
+    if (currentWeek > 2 ||
+        tminfo->tm_wday > 5 ||
+        tminfo->tm_hour > 15 ||
+        (tminfo->tm_hour < 8 && tminfo->tm_min < 30) {
+            return 50;
+    }
     index += (currentWeek - 1) * 25;
-
+    index += (tminfo->tm_wday - 1) * 5;
+    if (tminfo->tm_hour > 9 && tminfo->tm_min > 30) { index++; }
+    if (tminfo->tm_hour > 11 && tminfo->tm_min > 10) { index++; }
+    if (tminfo->tm_hour > 12 && tminfo->tm_min > 10) { index++; }
+    if (tminfo->tm_hour > 14) { index++; }
+    
     return index;
 }
